18f58b4d93977a7ee708f66d721c870f4701f614
Ajay <ajaydeepak07@gmail.com>
2018-12-12 23:34:38 +0530

    AppVersion Utils

---
+++ app/src/main/java/ajay/opensource/kotlinutils/AppVersion.kt
@@ -0,0 +1,19 @@
+package ajay.opensource.kotlinutils
+
+import android.content.Context
+
+/**
+ * Created by Ajay Deepak on 12-12-2018.
+ */
+
+/**
+ * get version code of this application
+ * @return version code
+ */
+fun Context.versionCode(): Int = getValue({ this.packageManager.getPackageInfo(this.packageName, 0).versionCode }, 0)
+
+/**
+ * get version name of this application
+ * @return version code
+ */
+fun Context.versionName(): String = getValue({ this.packageManager.getPackageInfo(this.packageName, 0).versionName }, "")
