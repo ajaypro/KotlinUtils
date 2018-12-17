3ebff544ae7121f1765fd7cc21a6ce37ec8bb2e7
Ajay <ajaydeepak07@gmail.com>
2018-12-18 01:32:13 +0530

    More Bitmap Util

--- app/src/main/java/ajay/opensource/kotlinutils/BitmapUtil.kt
+++ app/src/main/java/ajay/opensource/kotlinutils/BitmapUtil.kt
@@ -1,8 +1,11 @@
 package ajay.opensource.kotlinutils
 
+import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Matrix
+import android.net.Uri
 import java.io.File
 
 /**
@@ -41,4 +44,49 @@ fun File.saveBitmapToFile(bitmap:Bitmap,
     }
 
     return this
+}
+
+/**
+ * Save Bitmap to generated File
+ */
+@JvmOverloads
+fun Context.saveBitmapToFile(bitmap: Bitmap,
+                             dateFormat: String = "yyyy-MM-dd HH:mm:ss",
+                             format: Bitmap.CompressFormat = Bitmap.CompressFormat.JPEG,
+                             quality: Int = 100): File? {
+    val file = getOutputMediaFile(dateFormat, format)
+    file.saveBitmapToFile(bitmap, format, quality)
+    return file
+}
+
+/**
+ * Generate [File] object in externalFilesDir
+ */
+@JvmOverloads
+fun Context.getOutputMediaFile(dateFormat: String = "yyyy-MM-dd HH:mm:ss",
+                               format: Bitmap.CompressFormat = Bitmap.CompressFormat.JPEG): File {
+    val fileExtension = when (format) {
+        Bitmap.CompressFormat.JPEG -> ".jpg"
+        Bitmap.CompressFormat.PNG -> ".png"
+        Bitmap.CompressFormat.WEBP -> ".webp"
+    }
+    val picName = dateFromCurrentTime(dateFormat) + fileExtension
+    val folder = this.getExternalFilesDir(null)
+    if(!folder.isDirectory) {
+        folder.mkdirs()
+    }
+
+    return File(folder, picName)
+}
+
+/**
+ * Request MediaScanning
+ *
+ * @param[url] to request
+ */
+fun Context.requestMediaScanning(url: String) {
+    val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+    val contentUri = Uri.fromFile(File(url))
+    mediaScanIntent.data = contentUri
+    this.sendBroadcast(mediaScanIntent)
 }
